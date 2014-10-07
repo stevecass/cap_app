196fa6e8a1126d500a5368a967b62f8b69711148
Steven Cassidy <steven@mumsnet.com>
2014-10-06 23:59:35 -0400

    Allow creation of cows

--- app/controllers/cows_controller.rb
+++ app/controllers/cows_controller.rb
@@ -5,4 +5,20 @@ class CowsController < ApplicationController
     @request_host = request.host
     @db_info = ActiveRecord::Base.connection.adapter_name
   end
+
+  def new
+    @cow = Cow.new
+  end
+
+  def create
+    cow = Cow.create cow_params
+    redirect_to root_path
+  end
+
+  private
+
+  def cow_params
+     params.require(:cow).permit([:name, :age_in_years, :breed])
+  end
+
 end
